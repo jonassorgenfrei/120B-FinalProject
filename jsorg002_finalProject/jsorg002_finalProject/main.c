a977993bce1ac7eaee5f805a8d492d0a75a0c40b
Jonas <jonas_sorgenfrei@yahoo.de>
2018-05-22 09:41:30 -0700

    created Project

---
+++ jsorg002_finalProject/jsorg002_finalProject/main.c
@@ -0,0 +1,18 @@
+/*
+ * jsorg002_finalProject.c
+ *
+ * Created: 22.05.2018 09:40:43
+ * Author : Jonas
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
